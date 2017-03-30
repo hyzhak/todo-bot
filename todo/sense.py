940b894c84e12cf36029e10f2a04cd624c6d4956
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-03-30 11:37:40 +0200

    split enum of words to list of entites

--- todo/sense.py
+++ todo/sense.py
@@ -1,7 +1,48 @@
+import re
+
+tokenizer_reg = re.compile(r'([\W]+)')
+
+
+def tokenize(text):
+    return tokenizer_reg.split(text)
+
+
+def strip_tokens(tokens):
+    return [t.strip() for t in tokens]
+
+
+LINKED_WORDS = [',', '.', 'and', 'or', '...', '/']
+
+
+def split_by_linked_words(tokens):
+    sentence = []
+    for t in tokens:
+        if t in LINKED_WORDS:
+            yield sentence
+            sentence = []
+        else:
+            sentence.append(t)
+    yield sentence
+
+
+def flatten_sentences(tokens):
+    return [' '.join(t) for t in tokens]
+
+
+def strip_empty_words(tokens):
+    return [t for t in tokens if t.strip() != '']
+
+
 def extract_sense(text):
+    tokens = tokenize(text.strip())
+    tokens = strip_tokens(tokens)
+    tokens = strip_empty_words(tokens)
+    sentences = split_by_linked_words(tokens)
+    sentences = flatten_sentences(sentences)
+    sentences = strip_empty_words(sentences)
     return [{
         'intent': 'ADD_NEW_TASK',
         'entities': [{
-            'title': text,
-        }],
+                         'title': w,
+                     } for w in sentences],
     }]
--- todo/sense_test.py
+++ todo/sense_test.py
@@ -8,3 +8,16 @@ def test_regular_task_sentence():
             'title': 'flowers',
         }],
     }]
+
+
+def test_enumerate_tasks_sentence():
+    assert sense.extract_sense('candies, flowers and gift card') == [{
+        'intent': 'ADD_NEW_TASK',
+        'entities': [{
+            'title': 'candies',
+        }, {
+            'title': 'flowers',
+        }, {
+            'title': 'gift card',
+        }],
+    }]
